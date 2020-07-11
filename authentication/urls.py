3dd75059bbda727b4ab774b46c9843f81f1f9de0
bassasaurus <clearance.clarence@gmail.com>
2020-07-11 17:49:45 -0500

    removed sqlite2 from git

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from authenication.views import LoginView, LogoutView
 
+
 urlpatterns = [
-    path('accounts/profile/', ProfileView.as_view(), name='profile'),
+    # path('accounts/profile/', ProfileView.as_view(), name='profile'),
     path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_View(), name='logout',
+    path('accounts/logout/', LogoutView.as_View(), name='logout'),
     # path('accounts/password_change/', [name='password_change'],
     # path('accounts/password_change/done/', [name='password_change_done'],
     # path('accounts/password_reset/', [name='password_reset'],
