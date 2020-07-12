3b7bb8311218d9e38bb52df36fc0292b9a31d837
bassasaurus <clearance.clarence@gmail.com>
2020-07-12 18:21:16 -0500

    bg urls, views

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from transactions.views import HomeView, TransactionsListView, TransactionsCreateView, TransactionsDeleteView, TransactionsDetailView, TransactionsUpdateView
+from transactions.views import (
+            HomeView, TransactionsListView, TransactionsCreateView,
+            TransactionsDeleteView, TransactionsDetailView, TransactionsUpdateView,
+            BusinessGroupsListView, BusinessGroupsCreateView, BusinessGroupsDeleteView,
+            BusinessGroupsDetailView, BusinessGroupsUpdateView,
+            )
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
@@ -8,4 +13,11 @@ urlpatterns = [
     path('detail/<pk>', TransactionsDetailView.as_view(), name='transactions_detail'),
     path('update/<pk>', TransactionsUpdateView.as_view(), name='transactions_update'),
     path('delete/<pk>', TransactionsDeleteView.as_view(), name='transactions_delete'),
+
+    path('business/list/', BusinessGroupsListView.as_view(), name='transactions_list'),
+    path('business/create/', BusinessGroupsCreateView.as_view(), name='transactions_create'),
+    path('business/detail/<pk>', BusinessGroupsDetailView.as_view(), name='transactions_detail'),
+    path('business/update/<pk>', BusinessGroupsUpdateView.as_view(), name='transactions_update'),
+    path('business/delete/<pk>', BusinessGroupsDeleteView.as_view(), name='transactions_delete'),
+
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import TemplateView
 from authentication.views import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
-from transactions.models import Transactions
+from BusinessGroups.models import Transactions, BusinessGroups, Accounts
 from django.urls import reverse_lazy
 
 
@@ -44,3 +44,40 @@ class TransactionsDeleteView(DeleteView):
     model = Transactions
     template_name = 'transactions/transactions_delete.html'
     success_url = reverse_lazy('transactions_list')
+
+
+
+class BusinessGroupsListView(ListView):
+    model = BusinessGroups
+    template_name = 'business_groups/business_groups_list.html'
+
+
+class BusinessGroupsCreateView(CreateView):
+    model = BusinessGroups
+    fields = ['name', 'amount', 'accounts', 'categories', 'business_groups', 'comments']
+    template_name = 'business_groups/business_groups_create.html'
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super(BusinessGroupsCreateView, self).form_valid(form)
+
+
+class BusinessGroupsDetailView(DetailView):
+    model = BusinessGroups
+    template_name = 'business_groups/business_groups_detail.html'
+
+
+class BusinessGroupsUpdateView(UpdateView):
+    model = BusinessGroups
+    fields = ['name', 'amount', 'accounts', 'categories', 'business_groups', 'comments']
+    template_name = 'business_groups/business_groups_update.html'
+
+    def form_valid(self, form):
+        form.instance.updated_by = self.request.user
+        return super(BusinessGroupsUpdateView, self).form_valid(form)
+
+
+class BusinessGroupsDeleteView(DeleteView):
+    model = BusinessGroups
+    template_name = 'business_groups/business_groups_delete.html'
+    success_url = reverse_lazy('business_groups_list')
