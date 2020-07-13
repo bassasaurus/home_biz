3622dbab42936c565e128970d3e7fa15b8b99390
bassasaurus <clearance.clarence@gmail.com>
2020-07-12 19:48:41 -0500

    auth flow complete

--- authentication/views.py
+++ authentication/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.views import (
-                LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
-                PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView,
-                PasswordResetView)
-
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 
 class LoginRequired(LoginRequiredMixin):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
-
-
-class LoginView(LoginView):
-
-    form_class = AuthenticationForm
-    template_name = 'registration/login.html'
-
-
-class LogoutView(LogoutView):
-
-    template_name = 'registration/logged_out.html'
