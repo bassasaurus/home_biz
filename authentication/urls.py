4ee295164b12b4a267878d0dad6e5faa7bb7c4dc
bassasaurus <clearance.clarence@gmail.com>
2020-07-15 14:41:04 -0400

    added basic navbar

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,1 @@
 from django.urls import path, include
-from authenication.views import LoginView, LogoutView
-
-
-urlpatterns = [
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_View(), name='logout')
-]
