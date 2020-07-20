86527bcfa30a1c8e7bfd535cec44266beabf9102
bassasaurus <clearance.clarence@gmail.com>
2020-07-19 21:15:53 -0400

    allowed hosts

--- home_biz/settings.py
+++ home_biz/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # test comment
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(" ")
 
 USE_TZ = True
 TIME_ZONE = 'America/Chicago'
