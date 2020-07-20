95c649767963abcc6a18ec2156e00cab2db6da5e
bassasaurus <clearance.clarence@gmail.com>
2020-07-20 15:49:10 -0400

    simple account updating complete

--- transactions/signals.py
+++ transactions/signals.py
@@ -1,22 +1,21 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from transactions.models import Transactions, Accounts, Categories, BusinessGroups
+from transactions.models import Transactions, Accounts
 
 
 @receiver(post_save, sender=Transactions)
 def update_accounts(sender, instance, dispatch_uid='update_accounts', **kwargs):
 
-    account = instance.accounts
+    account = Accounts.objects.get(pk=instance.accounts.pk)
 
     if instance.type == 'Debit':
-
-        updated_balance = account.balance - instance.amount
-
-        print(account.name, updated_balance)
-
+        updated_balance = instance.accounts.balance - instance.amount
+        account.balance = updated_balance
+        account.save()
+        print(account.name, account.balance)
 
     if instance.type == 'Credit':
-
         updated_balance = account.balance + instance.amount
-
-        print(account.name, updated_balance)
+        account.balance = updated_balance
+        account.save()
+        print(account.name, account.balance)
