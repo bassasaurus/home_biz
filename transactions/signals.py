2f2683e174a9eebe654569dcfb35885b8d059f5c
bassasaurus <clearance.clarence@gmail.com>
2020-07-20 16:18:44 -0400

    simple business group updating complete

--- transactions/signals.py
+++ transactions/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from transactions.models import Transactions, Accounts
+from transactions.models import Transactions, Accounts, BusinessGroups
 
 
 @receiver(post_save, sender=Transactions)
@@ -12,10 +12,25 @@ def update_accounts(sender, instance, dispatch_uid='update_accounts', **kwargs):
         updated_balance = instance.accounts.balance - instance.amount
         account.balance = updated_balance
         account.save()
-        print(account.name, account.balance)
+
 
     if instance.type == 'Credit':
         updated_balance = account.balance + instance.amount
         account.balance = updated_balance
         account.save()
-        print(account.name, account.balance)
+
+@receiver(post_save, sender=Transactions)
+def update_business_groups(sender, instance, dispatch_uid='update_business_groups', **kwargs):
+
+    business = BusinessGroups.objects.get(pk=instance.business_groups.pk)
+
+    if instance.type == 'Debit':
+        updated_balance = instance.businesss.balance - instance.amount
+        business.balance = updated_balance
+        business.save()
+
+
+    if instance.type == 'Credit':
+        updated_balance = business.balance + instance.amount
+        business.balance = updated_balance
+        business.save()
