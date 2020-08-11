39fdb10f50a436a3371134a0af0d289ee6448c32
bassasaurus <clearance.clarence@gmail.com>
2020-08-11 14:33:01 -0500

    autocomplete complete

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from transactions.models import Transactions
+from dal import autocomplete
 
 
 class DateInput(forms.DateInput):
@@ -8,10 +9,12 @@ class DateInput(forms.DateInput):
 
 class TransactionsForm(forms.ModelForm):
 
-
     class Meta:
         model = Transactions
         fields = ['name', 'date', 'amount', 'type', 'accounts', 'categories', 'business_groups', 'comments']
         widgets = {
-            'date': DateInput()
+            'date': DateInput(),
+            'accounts': autocomplete.ModelSelect2(url='accounts_autocomplete'),
+            'categories': autocomplete.ModelSelect2(url='categories_autocomplete'),
+            'business_groups': autocomplete.ModelSelect2(url='business_groups_autocomplete')
         }
